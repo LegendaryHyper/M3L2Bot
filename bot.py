5c9672b1f5a3f4a45c02df330d7a3de274b09b56
İbrahim Deniz Altun <lh999space@gmail.com>
2025-08-31 18:09:46 +0300

    Added comment lines

---
+++ bot.py
@@ -0,0 +1,223 @@
+import discord
+from discord.ext import commands
+from logic import DB_Manager
+from discord import ui, ButtonStyle
+from config import DATABASE, TOKEN # Kütüphane ve modüller
+
+intents = discord.Intents.default() 
+intents.messages = True
+intents.message_content = True # Intentler
+
+class TestModal(ui.Modal, title='Test başlık'):
+    # Modal pencerede metin alanları tanımlama
+    field_1 = ui.TextInput(label='Kısa metin')
+    field_2 = ui.TextInput(label='Uzun metin', style=discord.TextStyle.paragraph)
+
+    # Modal pencere istendiğinde çağrılan bir yöntem
+    async def on_submit(self, interaction: discord.Interaction):
+        # Girilen verilerle mesajı güncelleme
+        await interaction.message.edit(content=f'Kısa metin: {self.field_1.value}\n'
+                                               f'Uzun metin: {self.field_2.value}')
+        # Yanıtın daha önce gönderilip gönderilmediğini kontrol etme
+        if not interaction.response.is_done():
+            # Gecikmeli yanıt için hazırlık yapma
+            await interaction.response.defer()
+
+# Buton tanımlama
+class TestButton(ui.Button):
+    # Belirli özellikler sahip bir butonun başlatılması
+    def __init__(self, label="Test etiketi", style=ButtonStyle.blurple, row=0):
+        super().__init__(label=label, style=style, row=row)
+
+    # Butona basıldığında çağrılan bir yöntem
+    async def callback(self, interaction: discord.Interaction):
+        # Kullanıcıya doğrudan mesaj gönderme
+        await interaction.user.send("Bir butona bastınız")
+        # Butona basılan kanala bir mesaj gönderme
+        await interaction.message.channel.send("Bir butona bastınız")
+        # Modal pencereyi açma
+        await interaction.response.send_modal(TestModal())
+        # Basıldıktan sonra butonun stilini değiştirme
+        self.style = ButtonStyle.gray
+
+        # Yanıtın daha önce gönderilip gönderilmediğini kontrol etme
+        if not interaction.response.is_done():
+            # Gecikmeli yanıt için hazırlık yapma
+            await interaction.response.defer()
+
+# Buton içeren bir pencere (görünüm) nesnesi tanımlama
+class TestView(ui.View):
+    # Görünümü başlatma
+    def __init__(self):
+        super().__init__()
+        # Görünüme bir buton ekleme
+        self.add_item(TestButton(label="Test etiketi"))
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+manager = DB_Manager(DATABASE)
+
+@bot.event
+async def on_ready(): # Bot hazır olduğunda terminalde bildirir.
+    print(f'Bot hazır! {bot.user} olarak giriş yapıldı.')
+
+# Butonu gösteren bir komut
+@bot.command()
+async def test(ctx):
+    # Bir buton içeren görünüm ile mesaj gönderme
+    await ctx.send("Aşağıdaki butona tıklayın:", view=TestView())
+
+@bot.command(name='start') # (!start) komutu ile botun kendini tanıtması sağlanır.
+async def start_command(ctx):
+    await ctx.send("Merhaba! Ben bir proje yöneticisi botuyum.\nProjelerinizi ve onlara dair tüm bilgileri saklamanıza yardımcı olacağım! =)")
+    await info(ctx)
+
+@bot.command(name='info')
+async def info(ctx): # (!info) komutu ile diğer komutlara dair bilgi verdirtir.
+    await ctx.send("""
+Kullanabileceğiniz komutlar şunlardır:
+
+!new_project - yeni bir proje eklemek
+!projects - tüm projelerinizi listelemek
+!update_projects - proje verilerini güncellemek
+!skills - belirli bir projeye beceri eklemek
+!delete - bir projeyi silmek
+
+Ayrıca, proje adını yazarak projeyle ilgili tüm bilgilere göz atabilirsiniz!""")
+
+@bot.command(name='new_project')
+async def new_project(ctx): # (!new_project) komutu ile yönergeler verilir ve yeni proje girilmesi sağlanır.
+    await ctx.send("Lütfen projenin adını girin!")
+
+    def check(msg):
+        return msg.author == ctx.author and msg.channel == ctx.channel
+
+    name = await bot.wait_for('message', check=check)
+    data = [ctx.author.id, name.content]
+    await ctx.send("Lütfen projeye ait bağlantıyı gönderin!")
+    link = await bot.wait_for('message', check=check)
+    data.append(link.content)
+
+    statuses = [x[0] for x in manager.get_statuses()]
+    await ctx.send("Lütfen projenin mevcut durumunu girin!", delete_after=60.0)
+    await ctx.send("\n".join(statuses), delete_after=60.0)
+    
+    status = await bot.wait_for('message', check=check)
+    if status.content not in statuses:
+        await ctx.send("Seçtiğiniz durum listede bulunmuyor. Lütfen tekrar deneyin!", delete_after=60.0)
+        return
+
+    status_id = manager.get_status_id(status.content)
+    data.append(status_id)
+    manager.insert_project([tuple(data)])
+    await ctx.send("Proje kaydedildi")
+
+@bot.command(name='projects')
+async def get_projects(ctx): # (!projects) komutu ile projeleri döndürür.
+    user_id = ctx.author.id
+    projects = manager.get_projects(user_id)
+    if projects:
+        text = "\n".join([f"Project name: {x[2]} \nLink: {x[4]}\n" for x in projects])
+        await ctx.send(text)
+    else:
+        await ctx.send('Henüz herhangi bir projeniz yok!\nBir tane eklemeyi düşünün! !new_project komutunu kullanabilirsiniz.')
+
+@bot.command(name='skills') # (!skills) komutu ile bir projeye beceri ekleyebilirsiniz.
+async def skills(ctx):
+    user_id = ctx.author.id
+    projects = manager.get_projects(user_id)
+    if projects:
+        projects = [x[2] for x in projects]
+        await ctx.send('Bir beceri eklemek istediğiniz projeyi seçin')
+        await ctx.send("\n".join(projects))
+
+        def check(msg):
+            return msg.author == ctx.author and msg.channel == ctx.channel
+
+        project_name = await bot.wait_for('message', check=check)
+        if project_name.content not in projects:
+            await ctx.send('Bu projeye sahip değilsiniz, lütfen tekrar deneyin! Beceri eklemek istediğiniz projeyi seçin')
+            return
+
+        skills = [x[1] for x in manager.get_skills()]
+        await ctx.send('Bir beceri seçin')
+        await ctx.send("\n".join(skills))
+
+        skill = await bot.wait_for('message', check=check)
+        if skill.content not in skills:
+            await ctx.send('Görünüşe göre seçtiğiniz beceri listede yok! Lütfen tekrar deneyin! Bir beceri seçin')
+            return
+
+        manager.insert_skill(user_id, project_name.content, skill.content)
+        await ctx.send(f'{skill.content} becerisi {project_name.content} projesine eklendi')
+    else:
+        await ctx.send('Henüz herhangi bir projeniz yok!\nBir tane eklemeyi düşünün! !new_project komutunu kullanabilirsiniz.')
+
+@bot.command(name='delete') # (!delete) komutu ile proje silinir.
+async def delete_project(ctx):
+    user_id = ctx.author.id
+    projects = manager.get_projects(user_id)
+    if projects:
+        projects = [x[2] for x in projects]
+        await ctx.send("Silmek istediğiniz projeyi seçin")
+        await ctx.send("\n".join(projects))
+
+        def check(msg):
+            return msg.author == ctx.author and msg.channel == ctx.channel
+
+        project_name = await bot.wait_for('message', check=check)
+        if project_name.content not in projects:
+            await ctx.send('Bu projeye sahip değilsiniz, lütfen tekrar deneyin!')
+            return
+
+        project_id = manager.get_project_id(project_name.content, user_id)
+        manager.delete_project(user_id, project_id)
+        await ctx.send(f'{project_name.content} projesi veri tabanından silindi!')
+    else:
+        await ctx.send('Henüz herhangi bir projeniz yok!\nBir tane eklemeyi düşünün! !new_project komutunu kullanabilirsiniz.')
+
+@bot.command(name='update_projects') # (!update_projects) komutu ile proje güncellenir.
+async def update_projects(ctx):
+    user_id = ctx.author.id
+    projects = manager.get_projects(user_id)
+    if projects:
+        projects = [x[2] for x in projects]
+        await ctx.send("Güncellemek istediğiniz projeyi seçin")
+        await ctx.send("\n".join(projects))
+
+        def check(msg):
+            return msg.author == ctx.author and msg.channel == ctx.channel
+
+        project_name = await bot.wait_for('message', check=check)
+        if project_name.content not in projects:
+            await ctx.send("Bir hata oldu! Lütfen güncellemek istediğiniz projeyi tekrar seçin:")
+            return
+
+        await ctx.send("Projede neyi değiştirmek istersiniz?")
+        attributes = {'Proje adı': 'project_name', 'Açıklama': 'description', 'Proje bağlantısı': 'url', 'Proje durumu': 'status_id'}
+        await ctx.send("\n".join(attributes.keys()))
+
+        attribute = await bot.wait_for('message', check=check)
+        if attribute.content not in attributes:
+            await ctx.send("Hata oluştu! Lütfen tekrar deneyin!")
+            return
+
+        if attribute.content == 'Durum':
+            statuses = manager.get_statuses()
+            await ctx.send("Projeniz için yeni bir durum seçin")
+            await ctx.send("\n".join([x[0] for x in statuses]))
+            update_info = await bot.wait_for('message', check=check)
+            if update_info.content not in [x[0] for x in statuses]:
+                await ctx.send("Yanlış durum seçildi, lütfen tekrar deneyin!")
+                return
+            update_info = manager.get_status_id(update_info.content)
+        else:
+            await ctx.send(f"{attribute.content} için yeni bir değer girin")
+            update_info = await bot.wait_for('message', check=check)
+            update_info = update_info.content
+
+        manager.update_projects(attributes[attribute.content], (update_info, project_name.content, user_id))
+        await ctx.send("Tüm işlemler tamamlandı! Proje güncellendi!")
+    else:
+        await ctx.send('Henüz herhangi bir projeniz yok!\nBir tane eklemeyi düşünün! !new_project komutunu kullanabilirsiniz.')
+
+bot.run(TOKEN) # Bot TOKEN kullanılarak kaldırılır.
--- logic.py
+++ logic.py
@@ -1,59 +1,47 @@
 import sqlite3
-from config import DATABASE
+from config import DATABASE # Kütüphane ve modüller.
 
+# Yetenek ve proje durumu olasılıkları (veritabanından silinmesi halinde geri eklenebilir.)
 skills = [ (_,) for _ in (['Python', 'SQL', 'API', 'Discord'])]
 statuses = [ (_,) for _ in (['Prototip Oluşturma', 'Geliştirme Aşamasında', 'Tamamlandı, kullanıma hazır', 'Güncellendi', 'Tamamlandı, ancak bakımı yapılmadı'])]
 
-class DB_Manager:
+class DB_Manager: # Sınıf tanımı
     def __init__(self, database):
-        self.database = database
+        self.database = database # Veri tabanı dosyasının tanımının eklenmesi.
         
-    def create_tables(self):
+    def create_tables(self): # Tabloları oluşturma.
         conn = sqlite3.connect(self.database)
         with conn:
-            conn.execute('''CREATE TABLE projects (
-                            project_id INTEGER PRIMARY KEY,
-                            user_id INTEGER,
-                            project_name TEXT NOT NULL,
-                            description TEXT,
-                            url TEXT,
-                            status_id INTEGER,
-                            FOREIGN KEY(status_id) REFERENCES status(status_id)
-                        )''') 
+            
             conn.execute('''CREATE TABLE skills (
                             skill_id INTEGER PRIMARY KEY,
                             skill_name TEXT
                         )''')
-            conn.execute('''CREATE TABLE project_skills (
-                            project_id INTEGER,
-                            skill_id INTEGER,
-                            FOREIGN KEY(project_id) REFERENCES projects(project_id),
-                            FOREIGN KEY(skill_id) REFERENCES skills(skill_id)
-                        )''')
+            
             conn.execute('''CREATE TABLE status (
                             status_id INTEGER PRIMARY KEY,
                             status_name TEXT
                         )''')
             conn.commit()
 
-    def __executemany(self, sql, data):
+    def __executemany(self, sql, data): # Çoklu SQL çalıştırma fonksiyonu.
         conn = sqlite3.connect(self.database)
         with conn:
             conn.executemany(sql, data)
             conn.commit()
-    def __execute(self, sql):
+    def __execute(self, sql): # SQL çalıştırma fonksiyonu.
         conn = sqlite3.connect(self.database)
         with conn:
             conn.execute(sql)
             conn.commit()
-    def __select_data(self, sql, data = tuple()):
+    def __select_data(self, sql, data = tuple()): # Belirli bir kısmı seçip döndürür.
         conn = sqlite3.connect(self.database)
         with conn:
             cur = conn.cursor()
             cur.execute(sql, data)
             return cur.fetchall()
         
-    def default_insert(self):
+    def default_insert(self): # Varsayılan yetenek ve proje durumlarını veri tabanına ekler.
         sql = 'INSERT OR IGNORE INTO skills (skill_name) values(?)'
         data = skills
         self.__executemany(sql, data)
@@ -62,12 +50,11 @@ class DB_Manager:
         self.__executemany(sql, data)
 
 
-    def insert_project(self, data):
-        sql = "INSERT INTO projects (user_id, projects_name, url, status_id) values(?, ?, ?, ?)" # Doğru SQL sorgusunu buraya girin
-        self.__executemany(sql, data)
+    def insert_project(self, data): # Proje ekler.
+        sql = "INSERT INTO projects (user_id, project_name, url, status_id) values(?, ?, ?, ?)"
 
 
-    def insert_skill(self, user_id, project_name, skill):
+    def insert_skill(self, user_id, project_name, skill): # Projeye yetenek ekler.
         sql = 'SELECT project_id FROM projects WHERE project_name = ? AND user_id = ?'
         project_id = self.__select_data(sql, (project_name, user_id))[0][0]
         skill_id = self.__select_data('SELECT skill_id FROM skills WHERE skill_name = ?', (skill,))[0][0]
@@ -77,24 +64,24 @@ class DB_Manager:
 
 
     def get_statuses(self):
-        sql = "SELECT status_name from status" # Doğru SQL sorgusunu buraya girin
+        sql = "SELECT status_name from status"
         return self.__select_data(sql)
         
 
-    def get_status_id(self, status_name):
+    def get_status_id(self, status_name): # Proje durumunu döndürür.
         sql = 'SELECT status_id FROM status WHERE status_name = ?'
         res = self.__select_data(sql, (status_name,))
         if res: return res[0][0]
         else: return None
 
-    def get_projects(self, user_id):
-        sql = "SELECT * FROM projects WHERE user_id = ?" # Doğru SQL sorgusunu buraya girin
+    def get_projects(self, user_id): # Proje döndürür.
+        sql = "SELECT * FROM projects WHERE user_id = ?"
         return self.__select_data(sql, data = (user_id,))
         
-    def get_project_id(self, project_name, user_id):
+    def get_project_id(self, project_name, user_id): # Belirli bir projenin kimlik sayısını döndürür.
         return self.__select_data(sql='SELECT project_id FROM projects WHERE project_name = ? AND user_id = ?  ', data = (project_name, user_id,))[0][0]
         
-    def get_skills(self):
+    def get_skills(self): # Tüm olası yetenek girdilerini döndürür.
         return self.__select_data(sql='SELECT * FROM skills')
     
     def get_project_skills(self, project_name):
@@ -114,23 +101,24 @@ WHERE project_name=? AND user_id=?
         return self.__select_data(sql=sql, data = (project_name, user_id))
 
 
-    def update_projects(self, param, data):
-        sql = "UPDATE projects SET {param} = ? WHERE project_name = ? AND user_id = ?" # Doğru SQL sorgusunu buraya girin
-        self.__executemany(sql, [data]) 
+    def update_projects(self, param, data): # Proje tablosunda belirli bir parametreye yeni bir değer atar.
+        sql = "UPDATE projects SET {param} = ? WHERE project_name = ? AND user_id = ?"
+        self.__executemany(sql, [data])
 
 
-    def delete_project(self, user_id, project_id):
-        sql = "DELETE from projects WHERE user_id = ? AND project_id = ?" # Doğru SQL sorgusunu buraya girin
+    def delete_project(self, user_id, project_id): # Projeyi siler.
+        sql = "DELETE from projects WHERE user_id = ? AND project_id = ?"
         self.__executemany(sql, [(user_id, project_id)])
     
-    def delete_skill(self, project_id, skill_id):
-        sql = "DELETE from skills WHERE skill_id = ? AND project_id = ?" # Doğru SQL sorgusunu buraya girin
+    def delete_skill(self, project_id, skill_id): # (Bu project_skills olmayacak mıydı?)
+        sql = "DELETE from skills WHERE skill_id = ? AND project_id = ?"
         self.__executemany(sql, [(skill_id, project_id)])
-    def alter_projects(self, new_column_name, new_column_type):
+    def alter_projects(self, new_column_name, new_column_type): # Projeye belirli bir ad ve türde yeni bir satır ekler.
         sql = f"ALTER TABLE projects ADD COLUMN {new_column_name} {new_column_type}"
         self.__execute(sql)
 
 
 if __name__ == '__main__':
     manager = DB_Manager(DATABASE)
-    manager.alter_projects("screenshots", "TEXT")
+    # manager.create_tables()
+    # manager.default_insert()
